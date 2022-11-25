07a5ba01184dfed38edd11b4af86a295985e866b
paullmich28 <paullmich28@gmail.com>
2022-11-25 09:04:15 +0700

    3

---
+++ resources/views/extends/header.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NEXT</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <link href="https://fonts.cdnfonts.com/css/witcher-handwriting" rel="stylesheet">
+    <link rel="stylesheet" href="/css/style.css" type="text/css">
+</head>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NEXT</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-    <link href="https://fonts.cdnfonts.com/css/witcher-handwriting" rel="stylesheet">
-    <link rel="stylesheet" href="/css/style.css" type="text/css">
-</head>
-<body>
+@include('extends.header')
+<body id="bodyHome">
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
         <div class="container">
             <a class="navbar-brand" href="#" id="main">
@@ -23,42 +10,36 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
-                @if(Auth::guard('web')->check())
-                    <li class="nav-item dropdown mx-2">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        {{Auth::guard('web')->user()->name}}
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Profile Details</a></li>
-                            <li>
-                                <a class="dropdown-item" href="{{route('user_logout')}}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">
-                                Something else here
-                                </a>
-                            </li>
-                        </ul>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                    <form action="{{route('user_logout')}}" id="logout-form" method="POST">
-                        @csrf
-                    </form>
-                @else
-                    <a class="btn btn-outline-success mx-3" id="login" href="{{route('login')}}" role="button">
-                        <i class="bx bx-log-in mx-auto"></i>
-                        Login
-                    </a>
-                    <a class="btn btn-outline-primary mx-3" id="register" href="{{route('register')}}" role="button">
-                        <i class='bx bx-user-plus'></i>
-                        Sign Up
-                    </a>
-                @endif
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a href="#">
+                            <img src="images/ig.jpg" alt="instagram" width="45" height="45" class="rounded-circle border border-2 border-white">
+                        </a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a href="#">
+                            <img src="images/opensea.png" alt="opensea" width="45" height="45" class="rounded-circle border border-2 border-white">
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
     </nav>
+
+    <div class="container trigger">
+        <div class="overlay rounded">
+            <h3 class="desc">Lorem Ipsum dolor sit amet</h3>
+            <form action="#">
+                <input type="submit" value="Enter the world" class="redirect btn btn-warning mt-4">
+            </form>
+        </div>
+    </div>
+    
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -69,49 +50,17 @@
         <div class="carousel-inner">
             <div class="carousel-item active c-item" data-bs-interval="5000">
                 <img src="/images/assetBG1.jpg" class="d-block w-100 c-img" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5 data-aos="fade-down" data-aos-duration="1000">Welcome to our website...</h5>
-                    <p data-aos="fade-up" data-aos-duration="1000">It seems like you're the chosen one...</p>
-                </div>
             </div>
             <div class="carousel-item c-item" data-bs-interval="5000">
                 <img src="/images/assetBG2.jpg" class="d-block w-100 c-img" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Right! You're NEXT!</h5>
-                    <p>
-                        <a href="#" class="text-white">Click</a> here!
-                    </p>
-                </div>
             </div>
             <div class="carousel-item c-item" data-bs-interval="5000">
                 <img src="/images/assetBG3.jpg" class="d-block w-100 c-img" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Are you curious what is this all about?</h5>
-                    <a class="btn btn-primary mx-auto mt-3 buttons" href="daftar.php" role="button">
-                        <i class='bx bx-user-plus'></i>
-                        SIGN UP
-                    </a>
-                </div>
             </div>
             <div class="carousel-item c-item">
                 <img src="/images/assetBG4.jpg" class="d-block w-100 c-img" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Sudah memiliki akun?</h5>
-                    <a class="btn btn-success mx-auto mt-3 buttons" href="{{route('login')}}" role="button">
-                        <i class="bx bx-log-in mx-auto"></i>
-                        LOGIN
-                    </a>
-                </div>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
     </div>
 
     <div class="positioning1">
@@ -128,25 +77,8 @@
     <section class="aboutus section-padding bg-info">
     </section>
     
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
     <script>
-        AOS.init();
-    </script>
-    <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/637af4deb0d6371309d023bc/1gic4p25m';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-        })();
-    </script>
-    <script>
-        function checkingWindow(){
+        /* function checkingWindow(){
             let login = document.querySelector("#login");
             let register = document.querySelector("#register");
     
@@ -171,7 +103,7 @@
                 register.classList.remove("mx-auto");
                 register.classList.remove("mt-2");
             }
-        }
+        } */
     
         $(window).scroll(function(){
             $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
@@ -180,5 +112,4 @@
         window.onload = checkingWindow;
         setInterval(checkingWindow, 500);
     </script>
-</body>
-</html>
+@include('extends.footer')
